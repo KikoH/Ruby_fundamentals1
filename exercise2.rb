7976c749b2ef74e710dca0555e339e6ee9f197c4
Kiko <mmhabib11@gmail.com>
2014-10-21 13:28:51 -0400

    Second exercise

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+#A good tip on 55 dollars
+puts 55 * 0.2
+
+#Adding a string to an integer
+puts "Hello " + 2.to_s
+
+#String interpolation
+x = 45628
+y = 7839
+puts "#{x} multiplied by #{y} equals #{x * y} "
+
+#Booleans
+puts (true && false)|| (false && true) || !(false && false) #that is equal to true
