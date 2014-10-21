e04b0f8699585d68d245ddbaa26764f3eca46144
Kiko <mmhabib11@gmail.com>
2014-10-21 13:54:29 -0400

    Getting age of user and outputting year born

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,9 @@
+#Asking for name
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}"
+#Asking for age
+puts " How old are you"
+#Converting year to an integer so that i could do a mathematical operation
+year = gets.to_i
+puts "You were born in #{2014 - year}"
